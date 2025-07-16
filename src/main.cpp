1779dd175f2656322b5132dfe74b2452c95a402b
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2025-07-16 10:51:36 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ static MatchSummary parse_match_json(const std::string &json, const std::string 
 }
 
 int main() {
-    const std::string apiKey = "YOUR_RIOT_API_KEY"; // set your key
+    const std::string apiKey = "RGAPI-67f4436d-e015-46ba-9462-aea26aceee2f"; // set your key
     const std::string region = "euw1";
     const std::string routing = "europe";
     const std::string summoner = "MoonFloww";
