ca61097d1e4bad6a35f206900a5248ebe1a093cd
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2025-07-18 10:50:53 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ static MatchSummary parse_match_json(const std::string &json, const std::string 
 }
 
 int main() {
-    const std::string apiKey = "RGAPI-67f4436d-e015-46ba-9462-aea26aceee2f"; // set your key
+    const std::string apiKey = "RGAPI-8bf31269-19a0-4f14-806d-638c06d4f1d6"; // 24hrs
     const std::string region = "euw1";
     const std::string routing = "europe";
     const std::string summoner = "MoonFloww";
